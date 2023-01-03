2d92a33ab931a5bcdf6f0e1d654917a2aff73630
Kuazk <allenz@M1-Mac-Allen.local>
2023-01-03 16:58:00 -0600

    update

---
+++ p1/response.mjs
@@ -0,0 +1,10 @@
+function decrypt(data) {
+    return 'd data'
+}
+function read(){
+    return decrypt('data');
+}
+
+export {
+    read,
+}
---
+++ p2/request.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+async function getData() {
+    try {
+        
+        setTimeout(() => console.log('hi'),0)
+
+        
+    } 
+    catch (error) { console.log('you have error')}
+    
+    //console.log(data)
+
+}
+getData();
+console.log('iam first')
